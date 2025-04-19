4ef63e4424319e840e786cbbb8c2586d0bb09d11
hakalochka <hakalochka@gmail.com>
2025-04-20 00:24:34 +0200

    add options menu

--- scripts/options_btn.gd
+++ scripts/options_btn.gd
@@ -3,3 +3,4 @@ extends TextureButton
 
 func _on_pressed() -> void:
 	SoundManager.play()
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
---
+++ scripts/options_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	if value == 0:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+	else:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	if value == 0:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), true)
+	else:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), false)
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(value))
