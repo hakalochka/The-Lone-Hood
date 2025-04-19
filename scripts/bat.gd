d389876dba63053027cb1e4f387cc8433832356b
hakalochka <hakalochka@gmail.com>
2025-04-19 23:05:58 +0200

    change enemy logic

--- scripts/bat.gd
+++ scripts/bat.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 
+
 @export var patrol_distance: float = 100.0
 @export var speed: float = 50.0
 
@@ -11,19 +12,26 @@ extends Node2D
 @onready var start_position = global_position
 
 func _physics_process(delta: float) -> void:
+	
 	if direction <= 0:
 		animated_sprite_2d.flip_h = true
 	else:
 		animated_sprite_2d.flip_h = false
 	
+	
+	
 	position.x += direction * speed * delta
 	var distance_from_start = global_position.x - start_position.x
 	
 	if ray_cast_right.is_colliding():
-		direction = -1
+		var collider = ray_cast_right.get_collider()
+		if collider and not collider.is_in_group("Player"):
+			direction = -1
 		
 	if ray_cast_left.is_colliding():
-		direction = 1
+		var collider = ray_cast_right.get_collider()
+		if collider and not collider.is_in_group("Player"):
+			direction = 1
 		
 		
 	if abs(distance_from_start) >= patrol_distance:
--- scripts/skullwolf.gd
+++ scripts/skullwolf.gd
@@ -1,35 +1,56 @@
-extends Node2D
+extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var hit_collision: CollisionShape2D = $hitBox/hitCollision
+@onready var attack_timer: Timer = $attackTimer
 
-@export var patrol_distance: float = 100.0
+@onready var player = get_node("/root/level_init/Player")
+
+#@export var patrol_distance: float = 100.0
 @export var speed: float = 50.0
+@export var chase_range := 150.0
+
 
 @onready var direction = -1 
-@onready var start_position = global_position
+var start_position := global_position
+
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
 	if direction <= 0:
-		animated_sprite_2d.flip_h = false
+		animated_sprite.flip_h = false
 	else:
-		animated_sprite_2d.flip_h = true
-	
-	position.x += direction * speed * delta
-	animated_sprite_2d.play("run")
-	var distance_from_start = global_position.x - start_position.x
-	
-	if ray_cast_right.is_colliding():
-		var collider = ray_cast_right.get_collider()
-		if collider and not collider.is_in_group("Player"):
-			direction = -1
-		
-	if ray_cast_left.is_colliding():
-		var collider = ray_cast_right.get_collider()
-		if collider and not collider.is_in_group("Player"):
+		animated_sprite.flip_h = true
+
+	var distance_to_player = global_position.distance_to(player.global_position)
+
+	if distance_to_player < chase_range:
+		if player.global_position.x > global_position.x:
 			direction = 1
-		
-		
-	if abs(distance_from_start) >= patrol_distance:
-		direction *= -1  
+		else:
+			direction = -1
+
+		velocity.x = direction * speed
+		animated_sprite.play("run")
+	else:
+		velocity.x = 0
+		animated_sprite.play("idle")
+	
+	move_and_slide()
+
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	attack_timer.start()
+
+func _on_attack_timer_timeout() -> void:
+	hit_collision.disabled = true
+	hit_collision.disabled = false
+	attack_timer.start()
+
+
+func _on_hit_box_body_exited(body: Node2D) -> void:
+	attack_timer.stop()
